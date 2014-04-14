e9ff7e0305403c33f0d07d173a2599aba049c21a
gurvinderd <dhillon.gurvinder@gmail.com>
2014-04-14 20:46:52 +0530

    1. Added TestNG Test and TestNG XML and executed successfully

--- src/com/webtest/webdrivertestngdemo.java
+++ src/com/webtest/webdrivertestngdemo.java
@@ -3,37 +3,51 @@
  */
 package com.webtest;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 /**
  * @author gurvinderd
  * This is first demo class for Webdriver
  */
-public class firstdemo {
+public class webdrivertestngdemo {
 
-	/**
-	 * @param args
-	 * @throws InterruptedException : thread.sleep throws exception
-	 */
-	public static void main(String[] args) throws InterruptedException {
-		WebDriver driver = new FirefoxDriver();
-		String baseUrl;
+	private WebDriver driver;
+	private String baseUrl;
+	/*private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();*/
+
+	@BeforeTest
+	public void setUp() throws Exception {
+		driver = new FirefoxDriver();
 		baseUrl = "http://newtours.demoaut.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+
+	@Test
+	public void testBookFlightTestNG() throws Exception {
+
+
 		driver.get(baseUrl + "/");
-		
+
 		//Login
 		driver.findElement(By.name("userName")).clear();
 		driver.findElement(By.name("userName")).sendKeys("mercury");
 		driver.findElement(By.name("password")).clear();
 		driver.findElement(By.name("password")).sendKeys("mercury");
 		driver.findElement(By.name("login")).click();
-		
+
 		// select nbr of tickets
 		new Select(driver.findElement(By.name("passCount"))).selectByVisibleText("2");
-		
+
 		//select from-to flight, date, class, and airline preference
 		new Select(driver.findElement(By.name("fromPort"))).selectByVisibleText("Frankfurt");
 		new Select(driver.findElement(By.name("fromMonth"))).selectByVisibleText("May");
@@ -47,7 +61,7 @@ public class firstdemo {
 		driver.findElement(By.cssSelector("input[name=\"findFlights\"]")).submit();
 		Thread.sleep(1000);
 		driver.findElement(By.name("reserveFlights")).click();
-		
+
 		// enter passenger detail
 		driver.findElement(By.name("passFirst0")).clear();
 		driver.findElement(By.name("passFirst0")).sendKeys("Gurvinder");
@@ -57,11 +71,11 @@ public class firstdemo {
 		driver.findElement(By.name("passFirst1")).sendKeys("MyName");
 		driver.findElement(By.name("passLast1")).clear();
 		driver.findElement(By.name("passLast1")).sendKeys("Dhillon");
-		
+
 		//enter meal preference
 		new Select(driver.findElement(By.name("pass.0.meal"))).selectByVisibleText("Hindu");
 		new Select(driver.findElement(By.name("pass.1.meal"))).selectByVisibleText("Vegetarian");
-		
+
 		// enter credit card information
 		new Select(driver.findElement(By.name("creditCard"))).selectByVisibleText("MasterCard");
 		driver.findElement(By.name("creditnumber")).clear();
@@ -76,12 +90,20 @@ public class firstdemo {
 		driver.findElement(By.name("cc_last_name")).sendKeys("LastN");
 		driver.findElement(By.name("buyFlights")).click();
 		System.out.println("flight booked");
-		
+
 		//return to flight booking page
 		driver.findElement(By.cssSelector("a > img")).click();
 		System.out.println("new booking page");
-		driver.close();
-		driver.quit();
+
 	}
 
+	@AfterTest
+	public void tearDown() throws Exception {
+		driver.close();		
+		driver.quit();
+		/*String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }*/
+	}
 }
